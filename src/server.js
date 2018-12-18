6cc93c166094ad44e26dbce56ab1ae2e9d9a5065
Arty <26905074+artysidorenko@users.noreply.github.com>
2018-12-18 12:11:52 +0000

    Include dummy handler in node server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
 var http = require('http');
-var server = http.createServer();
+
+var message = "I may not be a girl but I'm happy to be part of the node girls workshop";
+
+function handler (request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(message);
+  response.end();
+}
+
+var server = http.createServer(handler);
 server.listen(3000, function()  {
   console.log("Server listening on port 3000. Ready to accept requests!");
 });
