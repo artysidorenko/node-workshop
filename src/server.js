4fa971a781ced19e5bbcd6e96225e59401e18388
Arty <26905074+artysidorenko@users.noreply.github.com>
2018-12-18 13:09:50 +0000

    Expand handler to serve index.html page to client

--- src/server.js
+++ src/server.js
@@ -1,11 +1,31 @@
 var http = require('http');
+var fs = require('fs');
 
 var message = "I may not be a girl but I'm happy to be part of the node girls workshop";
 
 function handler (request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write(message);
-  response.end();
+
+  var endpoint = request.url;
+  console.log(endpoint);
+
+  var method = request.method;
+  console.log(method);
+
+  if(endpoint=="/node")  {message="I am Node!";}
+  if(endpoint=="/girl")  {message="I am (she) Node!";}
+
+  if(endpoint==="/")  {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    fs.readFile(__dirname + '/../public/index.html', function(error, file)  {
+      if (error)  {console.log(error); return;}
+      response.end(file);
+    });
+  }
+  else {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(message);
+    response.end();
+  }
 }
 
 var server = http.createServer(handler);
